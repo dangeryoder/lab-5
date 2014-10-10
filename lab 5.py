f0ca8d0393841354f9431c58b61bee9614eb073e
dangeryoder <dangeryoder@gmail.com>
2014-10-10 14:36:27 -0500

    lab 5

---
+++ lab 5.py
@@ -0,0 +1,30 @@
+# 70pt
+
+List = [1,2,3,4,5,6,7,8,9,10]
+
+for thing in List:
+    print thing
+
+# 80pt
+
+for item in range(11,21,1):
+    print item
+
+#90 pt
+
+for odd in range(100,0,-2):
+    print odd - 1
+    
+# 100pt
+
+List2 = []
+
+for userInput in range(0,5,1):
+    userInput = raw_input()
+    List2.append(userInput)
+    
+print "End of User Input"
+
+for i in List2:
+    print i
+
